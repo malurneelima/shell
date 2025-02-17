31776dbbc273c4162da83eea0836a039746391a5
Neelima Malur <neelima.malur@gmail.com>
2025-02-17 16:49:51 -0700

    I added new conversation scripts

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are you?"
+echo "Suresh: Hello Ramesh. I am fine. How are you doing?"
+echo "Ramesh: I am doing great Suresh. What's going on?"
+echo "Suresh: I am learning shell script Ramesh!"
