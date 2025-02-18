f11da4ba93af3d20b2ae5d28816874fdbf8e81ab
Neelima Malur <neelima.malur@gmail.com>
2025-02-17 20:15:53 -0700

    variables at runtime

--- variablesatrun.sh
+++ variablesatrun.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Please enter your username"
-read -s USERNAME #takes input into username variable. to hide what ever we are typing in theterminal, we can use -s.
+read  USERNAME #takes input into username variable. to hide what ever we are typing in theterminal, we can use -s.
 echo "The username you entered is $USERNAME"
+echo "Pleas enter your password"
+read -s PASSWORD
+echo "The password you entered is ${PASSWORD}"
