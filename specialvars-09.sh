e1dc5888f65eb2c3765f32725717dc64be199418
Neelima Malur <neelima.malur@gmail.com>
2025-02-18 16:41:02 -0700

    special variables

--- specialvars-09.sh
+++ specialvars-09.sh
@@ -4,3 +4,6 @@ echo "Number of variables passed to the script $#"
 echo "Script name is $0"
 echo "Current working directory $PWD"
 echo "Home directory of current user $HOME"
+echo "Process instance id of currentky executing script $$"
+sleep 100 &
+echo "Process instance if of last background command $!"
