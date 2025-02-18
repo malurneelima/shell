6b9a0c0eca7a454a41750cd3ad1a1abeb4b1161a
Neelima Malur <neelima.malur@gmail.com>
2025-02-17 20:28:43 -0700

    the datatypes array script

--- datatypes.sh
+++ datatypes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FRUITS=("APPLE" "KIWI" "ORANGE") # Here the size of array is 3 and the index starts with 0.
 
-echo "First fruit is $(FRUITS[0])"
-echo "Second fruit is $(FRUITS[1])"
-echo "Third fruit is $(FRUITS[2])"
-echo "All  fruits are $(FRUITS[@])" # @ will give all fruits
+echo "First fruit is ${FRUITS[0]}"
+echo "Second fruit is ${FRUITS[1]}"
+echo "Third fruit is ${FRUITS[2]}"
+echo "All  fruits are ${FRUITS[@]}" # @ will give all fruits
