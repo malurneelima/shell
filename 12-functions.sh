f15546fd632b6f644ad4447be8ddb49c0d2e2b23
Neelima Malur <neelima.malur@gmail.com>
2025-02-18 21:59:55 -0700

    adding functions script

--- 12-functions.sh
+++ 12-functions.sh
@@ -5,6 +5,13 @@ echo "User id is : $USERID"
 
 VALIDATE(){
     echo "The exit status is $1" #Here as validate function is sending the input details in the form of arguments, we are retriving using $1.
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is ....FAILED"
+        exit 1;
+    else
+        echo "$2 is ....SUCCESS"
+    fi
 }
 
 if [ $USERID -ne 0 ]
@@ -15,36 +22,25 @@ fi
 
 dnf list installed git
 
-VALIDATE $? # here making a calling to function and passing inputs as arguments
 
-# if [ $? -ne 0 ]
-# then
-#     echo "Git is not installed. Going to install now..."
-#     dnf install git -y
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "Git installation is not successful..Check it"
-#         exit 1;
-#     else
-#         echo "Git insatllation is successful"
-#     fi
-# else
-#     echo "Git is already installed. Nothing to do"
-# fi
 
-# dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed. Going to install now..."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
 
-# if [ $? -ne 0 ]
-# then
-#     echo "Mysql is not installed...Going to install"
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "mysql is not installed properly..Please check"
-#         exit 1
-#     else
-#         echo "My SQL is successfully installed"
-#     fi
-# else
-#     echo "Mysql is already installed.. Nothing to do"
-# fi
+else
+    echo "Git is already installed. Nothing to do"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed...Going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySql"
+else
+    echo "Mysql is already installed.. Nothing to do"
+fi
