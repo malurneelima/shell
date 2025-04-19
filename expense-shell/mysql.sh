46b19b40861c40d4c515131fe5d8dbf495d2a9a6
Neelima Malur <neelima.malur@gmail.com>
2025-04-19 17:47:15 -0400

    Adding expense project for mysql shell

---
+++ expense-shell/mysql.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LOGFOLDER="/var/log/shell-expense"
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGFOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGFOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root privileges $N" |tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"
+    fi
+}
+echo "Script started executing at $(date)"
+CHECK_ROOT
+
+dnf install mysql-server -y &>>LOG_FILE
+VALIDATE $? "Installing MYSQL Server"
+
+systemctl enable mysqld &>>LOG_FILE
+VALIDATE $? "Enabled MYSQL Server"
+
+systemctl start mysqld &>>LOG_FILE
+VALIDATE $? "Started MYSQL Server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOG_FILE
+VALIDATE $? "Setting up root password"
+
+
+
+
