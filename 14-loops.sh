b65c337529bd6b05dfae0850fbdd6b13cb4eb086
Neelima Malur <neelima.malur@gmail.com>
2025-04-19 05:50:59 -0400

    loops in shellscript and colors change

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,38 +1,7 @@
 #!/bin/bash
-for i in {0..3}
+for i in {0..100}
 do
     echo $i
-done
-
-
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "Please run this script with root privileges"
-        exit 1
-    fi
-}
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is...$R FAILED $N"
-        exit 1
-    else
-        echo -e "$2 is... $G SUCCESS $N"
-    fi
-}
-
-CHECK_ROOT
-# We can execute as sh 14-loops.sh git mysql ..
-for package in $@
-do
-    echo $package
 done
 
 
