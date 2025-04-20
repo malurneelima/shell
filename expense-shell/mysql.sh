fcda284a7776d129bbabf14a095e8a454d4d95cc
Neelima Malur <neelima.malur@gmail.com>
2025-04-20 06:34:13 -0400

    Adding expense project for mysql shell

--- expense-shell/mysql.sh
+++ expense-shell/mysql.sh
@@ -23,13 +23,13 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N"
+        echo -e "$2 is...$R FAILED $N"|tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is... $G SUCCESS $N"
+        echo -e "$2 is... $G SUCCESS $N"|tee -a $LOG_FILE
     fi
 }
-echo "Script started executing at $(date)"
+echo "Script started executing at $(date)"|tee -a $LOG_FILE
 CHECK_ROOT
 
 dnf install mysql-server -y &>>LOG_FILE
