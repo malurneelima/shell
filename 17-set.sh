8f80a1c2f332c7c3ebee100ea576c2e9766b6681
Neelima Malur <neelima.malur@gmail.com>
2025-04-22 17:23:01 -0400

    set code

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#set -e # setting the automatic exit if we get error
+set -e # setting the automatic exit if we get error
 echo "Hello World"
 echooo "Hello World failure"
 echo "Hello world after failure"
