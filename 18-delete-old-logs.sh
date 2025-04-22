4e72e0b2b8d847ed7bd264f1db503a3281836760
Neelima Malur <neelima.malur@gmail.com>
2025-04-22 18:13:26 -0400

    delete old logs usecase

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
  
- SOURCE_DIR="/home/ec2-user/logs"
+SOURCE_DIR="/home/ec2-user/logs"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
- if [ -d $SOURCE_DIR ]
- then
-    echo "$SOURCE_DIR exists"
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR $G Exists $N"
 else
-    echo "$SOURCE_DIR does not exists"
+    echo -e "$SOURCE_DIR $R does not exist $N"
+    exit 1
 fi
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files : $FILES"
