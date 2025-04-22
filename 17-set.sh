04c7244a22bf912e7348ced90963bd26ab36a757
Neelima Malur <neelima.malur@gmail.com>
2025-04-22 17:39:03 -0400

    set code

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -ex # setting the automatic exit if we get error.-e-->Exit immediately if any command returns a non-zero (error) status.and -x -->Print each command, USeful for debugging.
+failure(){
+    echo "Failed at $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal. if we get any error then trap command will catch it.
+
 echo "Hello World"
 echooo "Hello World failure"
 echo "Hello world after failure"
