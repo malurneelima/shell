f2912f578d04a8b15ce9dc2bf18a69ac81010791
Neelima Malur <neelima.malur@gmail.com>
2025-04-22 17:24:57 -0400

    set code

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e # setting the automatic exit if we get error
+set -ex # setting the automatic exit if we get error.-e-->Exit immediately if any command returns a non-zero (error) status.and -x -->Print each command, USeful for debugging.
 echo "Hello World"
 echooo "Hello World failure"
 echo "Hello world after failure"
