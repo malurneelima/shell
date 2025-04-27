9d0584b84d98aeb92cb9c49d04cbe5a23e9f7144
Neelima Malur <neelima.malur@gmail.com>
2025-04-27 16:42:44 -0400

    adding a comment

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -17,6 +17,7 @@ fi
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files : $FILES"
 
+#don't use line, it is reserved word.
 while IFS= read -r file #IFS means internal field separator,empty it will ignore white spaces. -r  is for not to ignore special chars like /.
 do 
     echo "Deleting file : $file"
